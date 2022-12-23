979a08c88c5a931afca59ef3ce20d64c832c263d
ManuelChavezH <adrian.acuna1305@gmail.com>
2022-12-22 23:58:49 -0500

    version3 problema2

--- prob_2/sol_1.c
+++ prob_2/sol_1.c
@@ -13,35 +13,27 @@ int main(){
         if((num < 580) && (num > 0)){
             f_num_valid = 1;
             printf("número valido!");
-        }
-        else {
-            if (num == 0){
-                break;
+            for (i = 1; i <= num; ++i) {
+                if (num % i == 0) {
+                    acum = acum + i;
+                }
             }
-            else {
-                f_num_valid=0;
-                printf("numero erroneo, vuelva a ingresar numero");
-            }
-        }
-    }
 
-        for (i = 1; i <= num; ++i) {
-            if (num % i == 0) {
-                acum = acum + i;
-            }
-        }
-
-        if((num == acum) && (num != 0)){
-            printf("%d es un numero PERFECTO",num);
-        } else {
-            if(num>acum){
+            if(num == acum){
+                printf("%d es un numero PERFECTO",num);    
+            } else if(num>acum){
                 printf("%d es un numero DEFICIENTE",num);
+            } else if(num<acum){
+                printf("%d es un numero ABUNDANTE",num); 
             }
-            else { 
-                if(num>acum){
-                    printf("%d es un numero ABUNDANTE",num); 
-                }
-                }
+
+        }
+        else if (num == 0){
+                break;
+        }
+        else {  f_num_valid=0;
+                printf("numero erroneo, vuelva a ingresar numero");
+        }    
     }
     printf("Eso es todo amigos");
     
